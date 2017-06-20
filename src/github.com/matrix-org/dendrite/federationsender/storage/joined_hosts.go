953c34fd4287dfe629ba109da2646c8a3281b434
Mark Haines <mjark@negativecurvature.net>
2017-06-20 11:00:08 +0100

    rename file and fix comment

--- src/github.com/matrix-org/dendrite/federationsender/storage/joined_hosts.go
+++ src/github.com/matrix-org/dendrite/federationsender/storage/joined_hosts.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package api provides the types that are used to communicate with the roomserver.
-
 package storage
 
 import (
